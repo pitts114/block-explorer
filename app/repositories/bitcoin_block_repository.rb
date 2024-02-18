3274c1c47b4552a4bc12adc989feef7f77e431a7
Justin <contact@justinpitts.dev>
2024-02-18 00:02:58 -0500

    Create BitcoinBlockRepository

---
+++ app/repositories/bitcoin_block_repository.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require Rails.root.join('lib/bitcoin/client')
+
+class BitcoinBlockRepository
+  def initialize(
+    client: Bitcoin::Client.new(
+      rpc_user: ENV['BITCOIN_RPC_USER'],
+      rpc_password: ENV['BITCOIN_RPC_PASSWORD'],
+      rpc_connect: ENV['BITCOIN_RPC_CONNECT'],
+      rpc_port: ENV['BITCOIN_RPC_PORT']
+    )
+  )
+    @client = client
+  end
+
+  def get_latest_blocks(count:)
+    return [] if count.zero?
+
+    current_block_count = @client.call_rpc_method(method: 'getblockcount')
+
+    ((current_block_count - count + 1)..current_block_count).reverse_each.map do |height|
+      block_hash = @client.call_rpc_method(method: 'getblockhash', params: [height])
+      @client.call_rpc_method(method: 'getblock', params: [block_hash, 1])
+    end
+  end
+end
---
+++ spec/repositories/bitcoin_block_repository_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe BitcoinBlockRepository do
+  subject(:repository) { described_class.new(client: client) }
+
+  let(:client) { double('BitcoinClient') }
+  let(:block) { double(:block) }
+  let(:most_recent_block) { double(:most_recent_block) }
+
+  describe '#get_latest_blocks' do
+    let(:current_block_count) { 100 }
+
+    before do
+      allow(client).to receive(:call_rpc_method).with(method: 'getblockcount')
+                                                .and_return(current_block_count)
+      allow(client).to receive(:call_rpc_method).with(method: 'getblockhash', params: [100])
+                                                .and_return('block_hash')
+      allow(client).to receive(:call_rpc_method).with(method: 'getblock', params: ['block_hash', 1])
+                                                .and_return(most_recent_block)
+      allow(client).to receive(:call_rpc_method).with(method: 'getblockhash', params: [99])
+                                                .and_return('most_recent_block_hash_99')
+      allow(client).to receive(:call_rpc_method).with(method: 'getblock', params: ['most_recent_block_hash_99', 1])
+                                                .and_return(block)
+    end
+
+    it 'returns an array of latest blocks with the most recent block first' do
+      blocks = repository.get_latest_blocks(count: 2)
+
+      expect(blocks).to eq([most_recent_block, block])
+    end
+
+    it 'returns an empty array when count is 0' do
+      blocks = repository.get_latest_blocks(count: 0)
+
+      expect(blocks).to eq([])
+    end
+  end
+end
