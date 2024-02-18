6f5946e85dee4d1b2e4853e0fd56cb381ceacf50
Justin <contact@justinpitts.dev>
2024-02-18 02:04:32 -0500

    remove nil results when height of blockchain is low

--- app/repositories/bitcoin_block_repository.rb
+++ app/repositories/bitcoin_block_repository.rb
@@ -17,7 +17,7 @@ class BitcoinBlockRepository
 
     current_block_count = @client.call_rpc_method(method: 'getblockcount')['result']
 
-    ((current_block_count - count + 1)..current_block_count).reverse_each.map do |height|
+    ([(current_block_count - count + 1), 0].max..current_block_count).reverse_each.map do |height|
       block_hash = @client.call_rpc_method(method: 'getblockhash', params: [height])['result']
       @client.call_rpc_method(method: 'getblock', params: [block_hash, 1])['result']
     end
--- spec/repositories/bitcoin_block_repository_spec.rb
+++ spec/repositories/bitcoin_block_repository_spec.rb
@@ -5,35 +5,32 @@ require 'rails_helper'
 RSpec.describe BitcoinBlockRepository do
   subject(:repository) { described_class.new(client: client) }
 
-  let(:client) { double('BitcoinClient') }
-  let(:block) { double(:block) }
-  let(:most_recent_block) { double(:most_recent_block) }
+  let(:client) do
+    Bitcoin::Client.new(
+      rpc_connect: ENV['BITCOIN_RPC_CONNECT'],
+      rpc_port: ENV['BITCOIN_RPC_PORT'],
+      rpc_user: ENV['BITCOIN_RPC_USER'],
+      rpc_password: ENV['BITCOIN_RPC_PASSWORD']
+    )
+  end
 
   describe '#get_latest_blocks' do
-    let(:current_block_count) { { 'result' => 100 } }
-
-    before do
-      allow(client).to receive(:call_rpc_method).with(method: 'getblockcount')
-                                                .and_return(current_block_count)
-      allow(client).to receive(:call_rpc_method).with(method: 'getblockhash', params: [100])
-                                                .and_return('result' => 'block_hash')
-      allow(client).to receive(:call_rpc_method).with(method: 'getblock', params: ['block_hash', 1])
-                                                .and_return('result' => most_recent_block)
-      allow(client).to receive(:call_rpc_method).with(method: 'getblockhash', params: [99])
-                                                .and_return('result' => 'most_recent_block_hash_99')
-      allow(client).to receive(:call_rpc_method).with(method: 'getblock', params: ['most_recent_block_hash_99', 1])
-                                                .and_return('result' => block)
-    end
-
     it 'returns an array of latest blocks with the most recent block first' do
       blocks = repository.get_latest_blocks(count: 2)
+      expect(blocks.size).to eq(2)
+      expect(blocks[0]).to include('hash' => '77fd27020e1d6812b45cc7070ffabd4995497d7f2c680b04d265c89d7431855f')
+      expect(blocks[1]).to include('hash' => '0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206')
+    end
 
-      expect(blocks).to eq([most_recent_block, block])
+    it 'returns an array of latest blocks with size smaller than count when height is lower' do
+      blocks = repository.get_latest_blocks(count: 3)
+      expect(blocks.size).to eq(2)
+      expect(blocks[0]).to include('hash' => '77fd27020e1d6812b45cc7070ffabd4995497d7f2c680b04d265c89d7431855f')
+      expect(blocks[1]).to include('hash' => '0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206')
     end
 
     it 'returns an empty array when count is 0' do
       blocks = repository.get_latest_blocks(count: 0)
-
       expect(blocks).to eq([])
     end
   end
